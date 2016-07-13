9b2b806db5117b6842248ce2d9c71b9344728bc4
mh <1906747819@qq.com>
2016-07-13 13:06:41 +0800

    建立base页面

---
+++ resources/views/home/common/base.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width,maximum-scale=1.0, minimum-scale=1.0,user-scalable=no" />
+    <title>@yield('page_title', $settings['site_name'] .' - '. $settings['short_intro'])</title>
+    <meta name="keywords" content="{{ $settings['site_keywords'] }}" />
+    <meta name="description" content="{{ $settings['short_intro'] }}" />
+    @include('home.common.header-meta')
+    @section('head')
+    @show
+</head>
+<body>
+    @include('home.common.header')
+
+    @section('main')
+    @show
+
+    @include('home.common.footer')
+
+    @section('script')
+    @show
+</body>
+</html>
